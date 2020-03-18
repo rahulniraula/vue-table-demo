7de089dfaf7f11fc5519038b9edee1c8b744b064
Rahul Niraula <rahul.niraula7@live.com>
2020-03-18 22:31:29 +0545

    listed countries list

---
+++ src/components/country-list.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-table  :items="data" :except="['states']"/>
+    </div>
+</template>
+<script>
+import vTable from "geeklearners-vue-table";
+export default {
+    components:{vTable},
+    data:function(){
+        return {
+            data:[]
+        }
+    },
+    mounted:function(){
+        fetch("https://raw.githubusercontent.com/stefanbinder/countries-states/master/countries.json")
+        .then(resp=>resp.json())
+        .then(data=>{this.data=data})
+
+    }
+}
+</script>
